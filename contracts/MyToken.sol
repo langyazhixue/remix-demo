869592d1dafb124bd198e9520836355b0df4619b
langyazhixue <langyazhixue0412@outlook.com>
2025-06-10 11:07:43 +0800

    add  注释

--- contracts/MyToken.sol
+++ contracts/MyToken.sol
@@ -6,9 +6,11 @@ import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
 
 contract MyToken is ERC20, ERC20Permit {
     constructor() ERC20("MyToken", "MTK") ERC20Permit("MyToken") {}
+     //  this is a mint funciotn
     function mint(address to, uint256 value) external  {
         _mint(to, value);
     }
+    //  this is a burn funciotn
     function burn(uint256 value) external  {
         _burn(msg.sender, value);
     }
