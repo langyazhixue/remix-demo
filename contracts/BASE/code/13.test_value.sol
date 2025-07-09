f34a7abe478ffe010e543cca116f9cd0b5150925
langyazhixue <langyazhixue0412@outlook.com>
2025-07-09 14:52:39 +0800

    add test

---
+++ contracts/BASE/code/13.test_value.sol
@@ -0,0 +1,16 @@
+pragma solidity ^0.8.0;
+
+contract TransferExample {
+    // 使用 transfer()
+    function sendViaTransfer(address payable _to) public payable {
+        _to.transfer(msg.value);
+    }
+    // 使用 call() - 推荐方式
+    function sendViaCall(address payable _to) public payable {
+        (bool sent, ) = _to.call{value: msg.value}("");
+        require(sent, "Failed to send Ether");
+    }
+    
+    // 接收以太币的函数
+    receive() external payable {}
+}
